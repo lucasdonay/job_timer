5010085b8a6717f06c757b09576a65a2bfcaf521
Lucas Donay <lucas.miranda@unimeduberaba.com.br>
2024-02-22 12:17:27 -0300

    modular

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,13 +1,22 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/modules/home/home_module.dart';
+import 'package:job_timer/app/modules/login/login_module.dart';
+import 'package:job_timer/app/services/auth/auth_service.dart';
+import 'package:job_timer/app/services/auth/auth_service_impl.dart';
 
-import 'modules/splash/splash_page.dart';
+import 'modules/splash/splash_module.dart';
 
 class AppModule extends Module {
   @override
-  void binds(Injector i) {}
+  void binds(Injector i) {
+    i.addLazySingleton<AuthService>(AuthServiceImpl.new, key: 'AuthService');
+  }
 
   @override
   void routes(RouteManager r) {
-    r.child('/', child: (context) => const SplashPage());
+    r
+      ..module(Modular.initialRoute, module: SplashModule())
+      ..module('/login/', module: LoginModule())
+      ..module('/home', module: HomeModule());
   }
 }
